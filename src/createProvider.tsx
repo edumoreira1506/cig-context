94bf472f5b5dcfd046f9e200e119ff41eb3e4617
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-27 18:59:52 -0300

    Bump to 0.1.1

--- src/createProvider.tsx
+++ src/createProvider.tsx
@@ -1,10 +1,10 @@
 import React, { useMemo, useReducer, ReactNode } from 'react';
 import { Context } from 'use-context-selector';
 
-export default function createProvider<T, I>(context: Context<I>, initialState: T, reducer: React.ReducerWithoutAction<any>) {
+export default function createProvider<T, I>(context: Context<I>, initialState: T, reducer: (state: T, action: I) => T) {
   return ({ children }: { children: ReactNode }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
-    const store = useMemo(() => ({ ...state, dispatch }), [state]);
+    const store = useMemo(() => ({ ...state, dispatch }) as any, [state]);
   
     return <context.Provider value={store}>{children}</context.Provider>;
   };
