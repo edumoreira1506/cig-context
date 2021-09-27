33780ebbe959ba9b359aa5f6be6e50c3581447f6
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-27 19:26:12 -0300

    Remove babel

--- src/createProvider.tsx
+++ src/createProvider.tsx
@@ -1,10 +1,10 @@
-import React, { useMemo, useReducer, ReactNode } from 'react';
+import * as React from 'react';
 import { Context } from 'use-context-selector';
 
 export default function createProvider<T, I, J>(context: Context<J>, initialState: T, reducer: (state: T, action: I) => T) {
-  return ({ children }: { children: ReactNode }) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const store = useMemo(() => ({ ...state, dispatch }) as any, [state]);
+  return ({ children }: { children: React.ReactNode }) => {
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+    const store = React.useMemo(() => ({ ...state, dispatch }) as any, [state]);
   
     return <context.Provider value={store}>{children}</context.Provider>;
   };
